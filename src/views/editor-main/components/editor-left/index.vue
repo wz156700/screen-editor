d84862abf305d2304f120ab73a68ca532cb9d7a8
wz156700 <wz156700@noreply.gitcode.com>
2024-06-21 18:45:36 +0800

    开始元素的显隐功能构建

--- src/views/editor-main/components/editor-left/index.vue
+++ src/views/editor-main/components/editor-left/index.vue
@@ -16,43 +16,15 @@
         </div>
         <!-- 内容 -->
         <div class="td-editorLeft-content">
-            <!-- 左侧 -->
-            <div class="td-editorLeft-content-left">
-                <div class="td-editorLeft-content-left-item" :class="active === 'history' ? 'is-active' : ''"
-                    @click="updataActive('history')">
-                    <EditorIcon name="history" size="16px"></EditorIcon>
-                    <span>最近</span>
+            <div class="itemContent">
+                <div class="img">
+                    <img src="/img/bg.png" alt="">
                 </div>
-                <div class="td-editorLeft-content-left-item" :class="active === 'analysis' ? 'is-active' : ''"
-                    @click="updataActive('analysis')">
-                    <EditorIcon name="analysis" size="16px"></EditorIcon>
-                    <span>图表</span>
+
+                <div class="editText" v-if="showEditText">
+                    <el-input v-model="state.input" style="width: 240px" placeholder="Please input" />
                 </div>
-                <div class="td-editorLeft-content-left-item" :class="active === 'components' ? 'is-active' : ''"
-                    @click="updataActive('components')">
-                    <EditorIcon name="components" size="16px"></EditorIcon>
-                    <span>小组件</span>
-                </div>
-                <div class="td-editorLeft-content-left-item" :class="active === 'map' ? 'is-active' : ''"
-                    @click="updataActive('map')">
-                    <EditorIcon name="history" size="16px"></EditorIcon>
-                    <span>地图</span>
-                </div>
-            </div>
-            <!-- 右侧 -->
-            <div class="td-editorLeft-content-right">
-                <el-scrollbar height="100%">
-                    <div class="td-editorLeft-content-right-box">
-                        <historyList :domData="domData" v-if="active === 'history'" @selectItem="selectItem"
-                            :selectId="selectId">
-                        </historyList>
-                        <rightCom :menuList="mapData" v-if="active === 'map'"></rightCom>
-                        <rightCom :menuList="analysisData" v-if="active === 'analysis'"></rightCom>
-                        <rightCom :menuList="componentsData" v-if="active === 'components'"></rightCom>
-                        <rightCom :menuList="hTitleData" v-if="active === 'hTitle'"></rightCom>
-                        <rightCom :menuList="materialData" v-if="active === 'material'"></rightCom>
-                    </div>
-                </el-scrollbar>
+                <div class="text" v-else>123</div>
             </div>
         </div>
     </div>
@@ -63,16 +35,7 @@ import { ref, watch, reactive, computed } from "vue";
 
 import { useCounterStore } from '@/store/editor'
 const emit = defineEmits(["selectItem"]);
-const active = ref("analysis");
-const updataActive = (val) => {
-    active.value = val;
-};
 const userStore = useCounterStore()
-
-const state = reactive({
-    isShowLeftBar: computed(() => userStore.global.isShowLeftBar)
-})
-
 const props = defineProps({
     domData: {
         type: Object,
@@ -84,9 +47,11 @@ const props = defineProps({
     },
 });
 
-const selectItem = (item) => {
-    emit("selectItem", item);
-};
+const state = reactive({
+    isShowLeftBar: computed(() => userStore.global.isShowLeftBar), input: '', showEditText: false
+})
+
+
 
 const closeLeftBar = () => {
     userStore.global.isShowLeftBar = !userStore.global.isShowLeftBar
@@ -122,67 +87,45 @@ const closeLeftBar = () => {
         height: calc(100% - 36px);
         display: flex;
 
-        .td-editorLeft-content-left {
-            width: 3.75rem;
-            height: 100%;
-            padding: .3125rem;
-            box-sizing: border-box;
-            background: var(--td-editor-left-nav-one);
+        .itemContent {
+            display: flex;
+            width: 80%;
+            height: 2.5rem;
+            margin: 0.5rem auto;
+            outline: 1px solid red;
 
-            .td-editorLeft-content-left-item {
-                width: 100%;
-                height: 60px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                flex-wrap: wrap;
-                flex-direction: column;
-                border-radius: 2px;
-                margin-bottom: 5px;
-                cursor: pointer;
-
-                span {
-                    display: block;
-                    width: 100%;
-                    text-align: center;
-                    margin-top: 5px;
-                }
-            }
-        }
-
-        .td-editorLeft-content-right {
-            width: calc(100% - 60px);
-            height: 100%;
-            padding-top: 5px;
-            padding-left: 5px;
-            padding-right: 5px;
-            box-sizing: border-box;
-
-            .td-editorLeft-content-right-box {
-                width: 100%;
+            .img {
+                width: 30%;
                 height: 100%;
 
-                &:deep(.el-collapse-item__header) {
-                    padding-left: 10px;
-                    box-sizing: border-box;
-                    height: 40px;
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: contain;
                 }
+            }
 
-                &:deep(.el-collapse-item__content) {
-                    padding-bottom: 0;
-                }
+            .text,
+            .editText {
+                width: 70%;
+                line-height: 2.5rem;
+                color: #fff;
+                margin-left: 0.5rem;
 
-                &:deep(.el-collapse-item__wrap) {
-                    z-index: 1;
-                    background: transparent;
+            }
+
+            .editText {
+
+
+                input {
+                    width: 100%;
+                    height: 100%;
+                    outline: none;
+                    border: none;
                 }
             }
         }
     }
 
-    .is-active {
-        background: var(--td-editor-left-nav-bg);
-        color: var(--td-editor-left-nav-text);
-    }
 }
 </style>
